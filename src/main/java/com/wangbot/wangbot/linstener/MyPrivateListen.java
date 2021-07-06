93cf9a433f52dc3480c5a2e21035321fb584bfad
AnAloneJaver <971366405@qq.com>
2021-07-07 00:06:39 +0800

    init

--- src/main/java/com/wangbot/wangbot/linstener/MyPrivateListen.java
+++ src/main/java/com/wangbot/wangbot/linstener/MyPrivateListen.java
@@ -2,6 +2,7 @@ package com.wangbot.wangbot.linstener;
 
 import catcode.CatCodeUtil;
 import cn.hutool.core.util.StrUtil;
+import com.wangbot.wangbot.util.SenderMsgUtil;
 import love.forte.simbot.annotation.Filter;
 import love.forte.simbot.annotation.Listener;
 import love.forte.simbot.annotation.OnPrivate;
@@ -13,6 +14,9 @@ import love.forte.simbot.api.sender.Sender;
 import love.forte.simbot.filter.MatchType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class MyPrivateListen {
@@ -28,6 +32,11 @@ public class MyPrivateListen {
         this.messageContentBuilderFactory = messageContentBuilderFactory;
     }
 
+
+    private final String HELP_INFO = "#h || #help || #帮助 \n 会显示WangBot的基本用法 \n #图片 上传 名称[图片] \n " +
+            "#图片 类别 || 随机一张指定类别库的图片 \n #骰子 x <事件名称>|| 扔出 最大 X 的随机数 \n 返回值为: XXX 做 <事件名称> 的成功率为 x \n" +
+            "#info || 查看机器人版本等信息";
+
     /**
      * 私聊监听
      */
@@ -37,52 +46,30 @@ public class MyPrivateListen {
         // 获取消息正文。
         MessageContent msgContent = privateMsg.getMsgContent();
         String msg1 = msgContent.getMsg();
+        System.out.println(msg1);
+        String[] helpArr = {"h","help","帮助"};
+        List<String> helpList = Arrays.asList(helpArr);
         if(StrUtil.isNotBlank(msg1)){
-            msg1 = msg1.trim();
-            String[] s = msg1.split(" ");
-
+            msg1 = msg1.replace("#","").trim().toLowerCase();
+            if(helpList.contains(msg1)){
+                SenderMsgUtil.sendPrivateMsg(sender,privateMsg.getAccountInfo().getAccountCode(),HELP_INFO);
+            }else if("info".equals(msg1)){
+                SenderMsgUtil.sendPrivateMsg(sender,privateMsg.getAccountInfo().getAccountCode(),"WangBot serion: v 1.0.0");
+            }else if(msg1.startsWith("图片")){
+                dealWithPic(msg1,privateMsg,sender);
+            }
         }
+    }
 
 
-        // 向 privateMsg 的账号发送消息，消息为当前接收到的消息。
-        sender.sendPrivateMsg(privateMsg, msgContent);
-
-        // 再发送一个表情ID为'9'的表情。
-        // 方法1：使用消息构建器构建消息并发送
-        // 在绝大多数情况下，使用消息构建器所构建的消息正文 'MessageContent'
-        // 是用来发送消息最高效的选择。
-        // 相对的，MessageContentBuilder所提供的构建方法是十分有限的。
-
-        // 获取消息构建器
-        MessageContentBuilder msgBuilder = messageContentBuilderFactory.getMessageContentBuilder();
-        // 通过.text(...) 向builder中追加一句话。
-        // 通过.face(ID) 向builder中追加一个表情。
-        // 通过.build() 构建出最终消息。
-        MessageContent msg = msgBuilder.text("表情：").face(9).build();
-
-        // 直接通过这个msg发送。
-        sender.sendPrivateMsg(privateMsg, msg);
-
-        // 方法2：使用CAT码发送消息。
-        // 使用CAT码构建一个需要解析的消息是最灵活的，
-        // 但是相对的，它的效率并不是十分的可观，毕竟在这其中可能会涉及到很多的'解析'操作。
-
-        // 获取CAT码工具类实例
-        CatCodeUtil catCodeUtil = CatCodeUtil.getInstance();
-
-        // 构建一个类型为 'face', 参数为 'id=9' 的CAT码。
-        // 有很多方法。
-
-        // 1. 通过 codeBuilder 构建CAT码
-        // String cat1 = catCodeUtil.getStringCodeBuilder("face", false).key("id").value(9).build();
-
-        // 2. 通过CatCodeUtil.toCat 构建CAT码
-        // String cat2 = catCodeUtil.toCat("face", "id=9");
-
-        // 3. 通过模板构建CAT码
-//        String cat3 = catCodeUtil.getStringTemplate().face(9);
-//
-//        // 在cat码前增加一句 '表情' 并发送
-//        sender.sendPrivateMsg(privateMsg, "表情：" + cat3);
+    public void dealWithPic(String msg,PrivateMsg privateMsg, Sender sender){
+        if("上传".indexOf(msg) >= 0 ){
+            //拆分出图片信息
+            //TODO
+            SenderMsgUtil.sendPrivateMsg(sender,privateMsg.getAccountInfo().getAccountCode(),"上传成功");
+        }else{
+            //查找图片
+            SenderMsgUtil.sendPrivatePic(sender,privateMsg.getAccountInfo().getAccountCode(),"");
+        }
     }
 }
