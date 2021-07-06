f4002ff3a7170dd6c25040662e2b41e1ab1d95c0
hushaoxiang <hushaoxiang@zzstxx.com>
2021-07-06 18:17:17 +0800

    init

---
+++ src/main/java/com/wangbot/wangbot/WangBotApplication.java
@@ -0,0 +1,17 @@
+package com.wangbot.wangbot;
+
+import love.forte.simbot.spring.autoconfigure.EnableSimbot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@EnableSimbot
+@SpringBootApplication
+public class WangBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WangBotApplication.class, args);
+    }
+
+}
